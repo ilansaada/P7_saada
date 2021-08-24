b7a1a26ae45cafc32c285ec00d9fc2d66991be9c
Julie VIGNAUX <jvignauxpro@gmail.com>
2021-08-24 14:27:18 +0200

    Nouveau model user après génération CLI

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,24 +1,22 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('mysql::memory:');
-
-const User = sequelize.define('User', {
-  // Model attributes are defined here
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING(64),
-    allowNull: false,
-    is: /^[0-9a-f]{64}$/i
-},
-  isAdmin: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: false
-
-
-  }
-},);
-module.exports = User
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
