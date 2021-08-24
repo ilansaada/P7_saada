60fb8f019f6c79757da90ac962bd18aa415fdf9d
ilan saada <ilansaada24@gmail.com>
2021-08-24 10:39:31 +0200

    connexion sequelize ok

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const { Sequelize } = require('sequelize');
+
+/*Création d'une instance Sequelize*/
+const sequelize = new Sequelize('groupomania', 'ILAN', 'Mysql1234ilan', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+/*Connexion*/
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+
 /*-------------------------------------------Déf des CORS lien backend et frontend ------------------------------------------*/
 
 app.use((req, res, next) => {
