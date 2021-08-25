ed4f0895903879454094bc2e5fe733efa498cb32
ilan saada <ilansaada24@gmail.com>
2021-08-25 13:33:42 +0200

    Début de la Création de toutes les routes user:  CREER, MODIFIER, SUPPRIMER

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const sequelize = require("../models");
 const jwt = require('jsonwebtoken');
+/*----------------------------Export de la fonction sigunp pour la création d'un Utilisateur----------------------------*/
 
 exports.signup = async (req, res, next) => {
   /*validation sequelize*/
@@ -17,9 +18,12 @@ exports.signup = async (req, res, next) => {
     })
     .catch((error) => res.status(500).json({ error }));
 };
+/*----------------------------Export de la fonction login pour la connexion d'un utilisateur----------------------------*/
+
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
+  sequelize.User.findOne({ email: req.body.email} )
   .then(user => {
+    console.log(user)/*Il pense que c'est toujours ilan@email.com*/
     if (!user) {
       return res.status(401).json({ error: 'Utilisateur non trouvé !' });
     }
@@ -29,9 +33,9 @@ exports.login = (req, res, next) => {
           return res.status(401).json({ error: 'Mot de passe incorrect !' });
         }
         res.status(200).json({
-          userId: user._id,
+          userId: user.id,
           token: jwt.sign({
-              userId: user._id },
+              userId: user.id },
               'RANDOM_TOKEN_SECRET',
               { expiresIn: '24h' }
           )
@@ -41,4 +45,24 @@ exports.login = (req, res, next) => {
   })
   .catch(error => res.status(500).json({ error }));
 
+};
+/*----------------------------Export de la fonction modifyuser pour la modification d'un utilisateur----------------------------*/
+
+exports.modifyUser = (req, res, next) => {
+  sequelize.User.updateOne(
+    { id: req.body.id },
+    )
+    .then(() => res.status(200).json({ message: "utilisateur modifié !" }))
+    .catch((error) => res.status(404).json({ error }));
+};
+/*----------------------------Export de la fonction deleteUser pour la suppression d'un utilisateur----------------------------*/
+
+exports.deleteUser = (req, res, next) => {
+  sequelize.User.findOne({ id: req.body.id })
+    .then((user) => {
+      sequelize.User.deleteOne({ id: req.body.id })
+      .then(() => res.status(200).json({ message: "utilisateur supprimé !" }))
+      .catch((error) => res.status(404).json({ error }));
+    })
+    .catch((error) => res.status(404).json({ error: 'prq' }));
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,8 @@ const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
+router.put("/:id",userCtrl.modifyUser);
+router.delete("/:id", userCtrl.deleteUser);
 
 
 console.log('route');
