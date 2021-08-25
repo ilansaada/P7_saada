0f6cc09a1aeedb36cc700aeaf62882ab1b2bf3a8
ilan saada <ilansaada24@gmail.com>
2021-08-25 11:14:29 +0200

    Création  de la route login

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,27 @@
 const express = require('express');
 const app = express();
-const { Sequelize } = require('sequelize');
+
 const userRoutes = require('./routes/user');
-
-/*Création d'une instance Sequelize*/
-/*const sequelize = new Sequelize('groupomania', 'ILAN', 'Mysql1234ilan', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-/*Connexion*/
-
 
 
 /*-------------------------------------------Déf des CORS lien entre backend et frontend ------------------------------------------*/
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
-    next();
-  });
-  app.use(express.urlencoded({extended:true}));
-  app.use(express.json());
-  /*déclaration des routes*/
-  app.use('/api/auth', userRoutes);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+/*déclaration des routes*/
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
