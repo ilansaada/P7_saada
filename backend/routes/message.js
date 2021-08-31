5498f445591464a1d2cf4a9893b298db9da935b0
ilan saada <ilansaada24@gmail.com>
2021-08-31 12:31:06 +0200

    ajout du middleware auth

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const router = express.Router();
 const messageCtrl = require('../controllers/message');
-/*const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');*/
+const auth = require('../middleware/auth');
+/*const multer = require('../middleware/multer-config');*/
 
-router.post("/", /*auth, multer,*/ messageCtrl.createMessage);
+router.post("/", auth, /*multer,*/ messageCtrl.createMessage);
 
-router.put("/:id", /*auth, multer,*/ messageCtrl.modifyMessage);
+router.put("/:id", auth, /*multer,*/ messageCtrl.modifyMessage);
 
-router.delete("/:id", /*auth,*/ messageCtrl.deleteMessage);
+router.delete("/:id", auth, messageCtrl.deleteMessage);
 
-router.get("/", /*auth,*/ messageCtrl.getAllMessage);
+router.get("/", auth, messageCtrl.getAllMessage);
 
-router.get("/:id", /*auth,*/ messageCtrl.getOneMessage);
+router.get("/:id", auth, messageCtrl.getOneMessage);
 
 
 
