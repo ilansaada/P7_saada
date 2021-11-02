b0f22b9d96ceef87647f7bb37751a223173ee22a
ilan saada <ilansaada24@gmail.com>
2021-11-02 12:46:15 +0100

    associations Commentaires Messages

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -11,6 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.Comment.belongsTo(models.Messgae,{
+        foreignKey:{
+          allowNull:false,
+        }
+        
+      })
     }
   };
   Comment.init({
