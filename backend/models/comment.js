daedb1603916038af661b038da37d02f0ef66ece
ilan saada <ilansaada24@gmail.com>
2021-11-02 14:53:13 +0100

    Correction de l'association message comment

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.Comment.belongsTo(models.Messgae,{
+      models.Comment.belongsTo(models.Message,{
         foreignKey:{
           allowNull:false,
         }
